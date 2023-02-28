a82c1842904856d1f4f053f475d78f610e3c71ec
AmondChocolateShake <dongju230130@gmail.com>
2023-02-28 12:51:07 +0900

    src

---
+++ ju.js
@@ -0,0 +1,18 @@
+
+
+
+const ju={
+  name:"dongju",
+  first:"kim",
+  age:23,
+  gender:"male",
+  printName:function(){
+    console.log(this.name);
+  },
+  printFName:function(){
+    console.log(this.first);
+  }
+
+}
+
+ju.printName();
---
+++ kim.js
@@ -0,0 +1,10 @@
+
+const kim={
+  adress:"sejong",
+  roadNum:"20032",
+  house:"garak",
+  printAdress:function(){
+    console.log(this.adress);
+
+  }
+}
